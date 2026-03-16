3dacb52fb37790e5c0f6dfc0b0d5b41f289b494b
Arajen Kajanthirabalan <arajenkajanthira@gmail.com>
2026-03-16 03:09:41 -0400

    connect app with backend and set up supabase connection

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,8 +1,9 @@
 import os
-
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
+load_dotenv()
 database_url =  os.getenv("DATABASE_URL")
 
-engine = create_async_engine(database_url)
+engine = create_async_engine(database_url,
+                             connect_args={"statement_cache_size": 0})
 async_session = async_sessionmaker(engine, expire_on_commit=False)
---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from db.models import Base
+from db.connection import engine
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup code here
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    # shutdown code here (optional)
+
+app = FastAPI(lifespan=lifespan)
